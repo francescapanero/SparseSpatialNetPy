3d823b81e99c6e0b4a820e035fca315baaaf7df4
Francesca <francesca.panero@spc.ox.ac.uk>
2021-04-27 19:49:11 +0200

    t=200 set up

--- multiple_experiments.py
+++ multiple_experiments.py
@@ -58,35 +58,9 @@ init[0] = {}
 #                         save_every=save_every, plot=True,  path='whyper_t50_L500',
 #                         save_out=False, save_data=False, init=init)
 
-t = 100
-
-G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=500)  # 400
-u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
-deg = np.array(list(dict(G.degree()).values()))
-ind = np.argsort(deg)
-index = ind[0:len(ind)-1]
-out = chain.mcmc_chains([G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
-                        w_inference='HMC', epsilon=0.01, R=5,
-                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='whyper_t100_L500',
-                        save_out=False, save_data=False, init=init)
-
-G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=1000)  # 650
-u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
-deg = np.array(list(dict(G.degree()).values()))
-ind = np.argsort(deg)
-index = ind[0:len(ind)-1]
-out = chain.mcmc_chains([G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
-                        w_inference='HMC', epsilon=0.01, R=5,
-                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='whyper_t100_L1000',
-                        save_out=False, save_data=False, init=init)
-
-# t = 200
+# t = 100
 #
-# G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=1000)  # 880
+# G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=500)  # 400
 # u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
 # deg = np.array(list(dict(G.degree()).values()))
 # ind = np.argsort(deg)
@@ -95,10 +69,10 @@ out = chain.mcmc_chains([G], iter, nburn, index,
 #                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
 #                         w_inference='HMC', epsilon=0.01, R=5,
 #                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-#                         save_every=save_every, plot=True,  path='whyper_t200_L1000',
+#                         save_every=save_every, plot=True,  path='whyper_t100_L500',
 #                         save_out=False, save_data=False, init=init)
 #
-# G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=2000)  # 1500
+# G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=1000)  # 650
 # u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
 # deg = np.array(list(dict(G.degree()).values()))
 # ind = np.argsort(deg)
@@ -107,8 +81,34 @@ out = chain.mcmc_chains([G], iter, nburn, index,
 #                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
 #                         w_inference='HMC', epsilon=0.01, R=5,
 #                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-#                         save_every=save_every, plot=True,  path='whyper_t200_L2000',
+#                         save_every=save_every, plot=True,  path='whyper_t100_L1000',
 #                         save_out=False, save_data=False, init=init)
+
+t = 200
+
+G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=1000)  # 880
+u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
+deg = np.array(list(dict(G.degree()).values()))
+ind = np.argsort(deg)
+index = ind[0:len(ind)-1]
+out = chain.mcmc_chains([G], iter, nburn, index,
+                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
+                        w_inference='HMC', epsilon=0.01, R=5,
+                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
+                        save_every=save_every, plot=True,  path='whyper_t200_L1000',
+                        save_out=False, save_data=False, init=init)
+
+G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=2000)  # 1500
+u = np.array([G.nodes[i]['u'] for i in range(G.number_of_nodes())])
+deg = np.array(list(dict(G.degree()).values()))
+ind = np.argsort(deg)
+index = ind[0:len(ind)-1]
+out = chain.mcmc_chains([G], iter, nburn, index,
+                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
+                        w_inference='HMC', epsilon=0.01, R=5,
+                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
+                        save_every=save_every, plot=True,  path='whyper_t200_L2000',
+                        save_out=False, save_data=False, init=init)
 
 
 
