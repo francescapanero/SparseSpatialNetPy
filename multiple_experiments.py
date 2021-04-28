3ab2b88104d90ef822017e03c62066d0038b5810
Francesca <francesca.panero@spc.ox.ac.uk>
2021-04-28 11:19:15 +0200

    t=100 and L1000 repeat

--- multiple_experiments.py
+++ multiple_experiments.py
@@ -58,28 +58,28 @@ init[0] = {}
 
 t = 100
 
-G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=500)  # 400
-deg = np.array(list(dict(G.degree()).values()))
-ind = np.argsort(deg)
-index = ind[0:len(ind)-1]
-
-iter = 400000
-nburn = int(iter * 0.25)
-out = chain.mcmc_chains([G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
-                        w_inference='HMC', epsilon=0.01, R=5,
-                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='whyper_t100_L500_second',
-                        save_out=False, save_data=False, init=init)
-
-iter = 1000000
-nburn = int(iter * 0.25)
-out = chain.mcmc_chains([G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=True, beta=False,
-                        w_inference='HMC', epsilon=0.01, R=5,
-                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='xwhyper_t100_L500',
-                        save_out=False, save_data=False, init=init)
+# G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=500)  # 400
+# deg = np.array(list(dict(G.degree()).values()))
+# ind = np.argsort(deg)
+# index = ind[0:len(ind)-1]
+#
+# iter = 400000
+# nburn = int(iter * 0.25)
+# out = chain.mcmc_chains([G], iter, nburn, index,
+#                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
+#                         w_inference='HMC', epsilon=0.01, R=5,
+#                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
+#                         save_every=save_every, plot=True,  path='whyper_t100_L500_second',
+#                         save_out=False, save_data=False, init=init)
+#
+# iter = 1000000
+# nburn = int(iter * 0.25)
+# out = chain.mcmc_chains([G], iter, nburn, index,
+#                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=True, beta=False,
+#                         w_inference='HMC', epsilon=0.01, R=5,
+#                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
+#                         save_every=save_every, plot=True,  path='xwhyper_t100_L500',
+#                         save_out=False, save_data=False, init=init)
 
 
 G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=1000)  # 650
@@ -93,7 +93,7 @@ out = chain.mcmc_chains([G], iter, nburn, index,
                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
                         w_inference='HMC', epsilon=0.01, R=5,
                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='whyper_t100_L1000_second',
+                        save_every=save_every, plot=True,  path='whyper_t100_L1000_third',
                         save_out=False, save_data=False, init=init)
 
 iter = 1000000
@@ -102,7 +102,7 @@ out = chain.mcmc_chains([G], iter, nburn, index,
                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=True, beta=False,
                         w_inference='HMC', epsilon=0.01, R=5,
                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='xwhyper_t100_L1000',
+                        save_every=save_every, plot=True,  path='xwhyper_t100_L1000_second',
                         save_out=False, save_data=False, init=init)
 
 # t = 200
