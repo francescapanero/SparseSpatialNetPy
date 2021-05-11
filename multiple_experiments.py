1f8a787f0d8604ef275256dd09fb66a1d1651fb6
Francesca <francesca.panero@spc.ox.ac.uk>
2021-05-11 21:59:44 +0200

    hyper w x right t200

--- multiple_experiments.py
+++ multiple_experiments.py
@@ -182,20 +182,20 @@ init[2]['sigma'] = 0.2
 init[2]['t'] = 100
 init[2]['c'] = 3
 
-out = chain.mcmc_chains([G, G, G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=False, n=False, u=False, x=False, beta=False,
-                        w_inference='HMC', epsilon=0.01, R=5,
-                        sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='L3000_gamma3_t200_hyper',
-                        save_out=False, save_data=False, init=init, a_t=500)
+# out = chain.mcmc_chains([G, G, G], iter, nburn, index,
+#                         sigma=True, c=True, t=True, tau=False, w0=False, n=False, u=False, x=False, beta=False,
+#                         w_inference='HMC', epsilon=0.01, R=5,
+#                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
+#                         save_every=save_every, plot=True,  path='L3000_gamma3_t200_hyper',
+#                         save_out=False, save_data=False, init=init, a_t=200)
 
-iter = 100000
+iter = 300000
 nburn = int(iter * 0.25)
 out = chain.mcmc_chains([G, G, G], iter, nburn, index,
-                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=False, beta=False,
+                        sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=True, beta=False,
                         w_inference='HMC', epsilon=0.01, R=5,
                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='L3000_gamma3_t200_hyper_w',
+                        save_every=save_every, plot=True,  path='L3000_gamma3_t200_hyper_w_x',
                         save_out=False, save_data=False, init=init)
 
 # iter = 500000
