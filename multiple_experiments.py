0af8a0af773b88d5f1acc41110b39d90c4c806eb
Francesca <francesca.panero@spc.ox.ac.uk>
2021-05-17 18:30:19 +0200

    space with true init

--- multiple_experiments.py
+++ multiple_experiments.py
@@ -163,6 +163,7 @@ gamma = 2
 
 G = GraphSampler(prior, approximation, sampler, sigma, c, t, tau, gamma, size_x, a_t, b_t, T=T, K=K, L=2000)
 deg = np.array(list(dict(G.degree()).values()))
+x = np.array([G.nodes[i]['x'] for i in range(G.number_of_nodes())])
 ind = np.argsort(deg)
 index = ind[0:len(ind)-1]
 
@@ -170,23 +171,24 @@ iter = 300000
 nburn = int(iter * 0.25)
 init = {}
 init[0] = {}
-init[0]['sigma'] = 0.8
-init[0]['t'] = 300
-init[0]['c'] = 2
+init[0]['sigma'] = sigma
+init[0]['t'] = t
+init[0]['c'] = c
+init[0]['x'] = x
 init[1] = {}
-init[1]['sigma'] = sigma
-init[1]['t'] = t
-init[1]['c'] = c
+init[1]['sigma'] = 0.8
+init[1]['t'] = 300
+init[1]['c'] = 2
 init[2] = {}
 init[2]['sigma'] = 0.2
 init[2]['t'] = 100
 init[2]['c'] = 3
 
-out = chain.mcmc_chains([G, G, G], iter, nburn, index,
+out = chain.mcmc_chains([G], iter, nburn, index,
                         sigma=True, c=True, t=True, tau=False, w0=True, n=False, u=False, x=True, beta=False,
                         w_inference='HMC', epsilon=0.01, R=5,
                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='L2000_gamma2_t200_hyper_w_x',
+                        save_every=save_every, plot=True,  path='L2000_gamma2_t200_hyper_w_x_trueinitx',
                         save_out=False, save_data=False, init=init, a_t=200)
 
 
