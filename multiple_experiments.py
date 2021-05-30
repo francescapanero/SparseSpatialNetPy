e74bc38ae18248312ff0a45d1c70c55f634bba0e
Francesca <francesca.panero@spc.ox.ac.uk>
2021-05-31 00:02:32 +0200

    longer positive deg L=2000

--- multiple_experiments.py
+++ multiple_experiments.py
@@ -167,30 +167,30 @@ x = np.array([G.nodes[i]['x'] for i in range(G.number_of_nodes())])
 w0 = np.array([G.nodes[i]['w0'] for i in range(G.number_of_nodes())])
 ind = np.argsort(deg)
 # index = ind[0:len(ind)-1]
-index = ind[-sum(deg>0)-1:-1]
+index = ind[-sum(deg>0):-1]
 p_ij = G.graph['distances']
 
 init = {}
-init[0] = {}
-init[0]['sigma'] = sigma
-init[0]['t'] = t
-init[0]['c'] = c
+# init[0] = {}
+# init[0]['sigma'] = sigma
+# init[0]['t'] = t
+# init[0]['c'] = c
+# init[0]['x'] = x.copy()
+# init[0] = {}
+init[0]['sigma'] = 0.8
+init[0]['t'] = 300
+init[0]['c'] = 2
 init[0]['x'] = x.copy()
-init[1] = {}
-init[1]['sigma'] = 0.8
-init[1]['t'] = 300
-init[1]['c'] = 2
-init[1]['x'] = x.copy()
-init[1]['x'][index] = size_x * np.random.rand(len(index))
+init[0]['x'][index] = size_x * np.random.rand(len(index))
 
 
-iter = 300000
+iter = 700000
 nburn = int(iter * 0.25)
-out = chain.mcmc_chains([G, G], iter, nburn, index,
+out = chain.mcmc_chains([G], iter, nburn, index,
                         sigma=True, c=True, t=True, tau=False, w0=False, n=False, u=False, x=True, beta=False,
                         w_inference='HMC', epsilon=0.01, R=5,
                         sigma_sigma=0.01, sigma_c=0.01, sigma_t=0.01, sigma_tau=0.01, sigma_x=0.01,
-                        save_every=save_every, plot=True,  path='positivedegnodes_x',
+                        save_every=save_every, plot=True,  path='positivedegnodes_x_longer',
                         save_out=False, save_data=False, init=init, a_t=200)
 
 
